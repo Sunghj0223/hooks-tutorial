df8f4185029ac9277d2088823bc1fe1b6d15d3bd
Sunghj0223 <Sunghj0223@gmail.com>
2022-08-16 00:14:25 +0900

    first_upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    // <React.StrictMode> // 이거 하면 console할때 두번씩 겹쳐서나와서 잠깐 꺼놨음.
+    <App />
+    // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
